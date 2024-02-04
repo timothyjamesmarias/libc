cbdc98670e99df5d65b86ec4d2621ac15af62a98
timothyjamesmarias <timothyjamesmarias@gmail.com>
2024-02-04 15:39:17 -0800

    test suite wip

--- test_string.c
+++ test_string.c
@@ -1,4 +1,19 @@
+#include "string.h"
+
 int test_memcmp() {
+  const char * s1 = "asdfasdfasdfasdf";
+  const char * s2 = "asdfasdfasdfasdf";
+  const char * s3 = "asdfasdf";
+  const char * s3 = "asdfasda";
+
+  // compare the same string, should return 0
+  int test1 = memcmp(s1, s2, 20);
+
+  // compare strings of different lengths, should return -1
+  int test2 = memcmp(s1, s3, 20);
+
+  // compare different strings, should return -1
+  int test3 = memcmp(s2, s3, 20);
 
 }
 
